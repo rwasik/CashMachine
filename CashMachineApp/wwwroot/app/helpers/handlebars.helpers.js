a5657c6b327bdc64b80776b95258aa57ecdec950
Robert Wąsik <robert.wasik@outlook.com>
2019-08-31 17:37:23 +0200

    Modify notes presentation

--- CashMachineApp/wwwroot/app/helpers/handlebars.helpers.js
+++ CashMachineApp/wwwroot/app/helpers/handlebars.helpers.js
@@ -1,6 +1,3 @@
-Handlebars.registerHelper('times', function (n, block) {
-    var accum = '';
-    for (var i = 0; i < n; ++i)
-        accum += block.fn(i);
-    return accum;
+Handlebars.registerHelper('notesResult', function (items, options) {
+    return options.fn(items.join(', '));
 });
--- CashMachineApp/wwwroot/app/notes/notes.manager.js
+++ CashMachineApp/wwwroot/app/notes/notes.manager.js
@@ -19,16 +19,35 @@ var notesManager = function () {
         updateBtnWithdrawDisabledState(false);
         updateSpinnerState(false);
 
+        var notes = getNotes(data);
+
         var source = document.getElementById('notes-template').innerHTML;
         var template = Handlebars.compile(source);
-        var html = template(data);
+        var html = template({ notes: notes, errorMessage: data.errorMessage });
 
         $('#result').append(html);
     };
 
+    var getNotes = function (data) {
+        var notes = [];
+
+        addNote(100, data.quantityOf100Notes, notes);
+        addNote(50, data.quantityOf50Notes, notes);
+        addNote(20, data.quantityOf20Notes, notes);
+        addNote(10, data.quantityOf10Notes, notes);
+
+        return notes;
+    };
+
+    var addNote = function (noteValue, quantity, collection) {
+        for (let i = 0; i < quantity; i++) {
+            collection.push(noteValue);
+        }
+    };
+
     var updateBtnWithdrawDisabledState = function (isDisabled) {
         $('#btnWithdraw').attr('disabled', isDisabled);
-    }
+    };
 
     var updateSpinnerState = function (isVisible) {
         if (isVisible) {
@@ -37,7 +56,7 @@ var notesManager = function () {
         else {
             $('#spinner').addClass('d-none');
         }
-    }
+    };
 
     return {
         enhanceWithdrawBtn: enhanceWithdrawBtn
